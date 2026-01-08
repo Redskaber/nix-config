593298cef8efea3fba553f23ee9a4938fb678015
redskaber <redskaber@foxmail.com>
2026-01-09 03:33:09 +0800

    (update): devshell nix develop github:redskaber/nix-config#python-machine

--- home-manager/dev/default.nix
+++ home-manager/dev/default.nix
@@ -2,6 +2,7 @@
 # @author: redskaber
 # @datetime: 2025-12-12
 # @description: Modern, fast C development environment with clangd + bear
+# @Tips: Only this file combinFrom base dev shell
 
 
 { pkgs, inputs, dev, ... }: {
@@ -30,6 +31,27 @@
     '';
     postShellHook = ''
       echo "[postShellHook]: default shell!"
+    '';
+  };
+  cpython = {
+    combinFrom = [
+      dev.c
+      dev.cpp
+      dev.python
+    ];
+    buildInputs = with pkgs; [];
+    nativeBuildInputs = with pkgs; [];
+    preInputsHook = ''
+      echo "[preInputsHook]: cpython shell!"
+    '';
+    postInputsHook = ''
+      echo "[postInputsHook]: cpython shell!"
+    '';
+    preShellHook = ''
+      echo "[preShellHook]: cpython shell!"
+    '';
+    postShellHook = ''
+      echo "[postShellHook]: cpython shell!"
     '';
   };
 }
