b4f4a6e330e5d7a5e321d6441ad36eda1b27ffe5
redskaber <redskaber@foxmail.com>
2026-01-10 20:37:33 +0800

    (chore): add non-nixos sup

--- home-manager/srv/mako.nix
+++ home-manager/srv/mako.nix
@@ -4,30 +4,38 @@
 # @diractory: https://nix-community.github.io/home-manager/options.xhtml#opt-services.mako.enable
 
 
-services.mako = {
-  enable = true;
-  settings = {
-    # global
-    font = "JetBrainsMono Nerd Font 10";
-    background-color = "#1e1e2e";
-    text-color = "#cdd6f4";
-    border-color = "#89b4fa";
-    border-radius = 8;
-    margin = "10,10,10,10";  # top,right,bottom,left
-    padding = "10,10";
-    default-timeout = 5000;  # show 5s
-    layer = "overlay";       # show top layer (Wayland)
-    icons = true;
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
 
-    # Optional: urgency group css
-    "urgency=low" = {
-      background-color = "#313244";
-    };
-    "urgency=critical" = {
-      background-color = "#eba0ac";
-      text-color = "#111111";
-      default-timeout = 0;  # don't auto shadow
+  services.mako = {
+    enable = true;
+    settings = {
+      # global
+      font = "JetBrainsMono Nerd Font 10";
+      background-color = "#1e1e2e";
+      text-color = "#cdd6f4";
+      border-color = "#89b4fa";
+      border-radius = 8;
+      margin = "10,10,10,10";  # top,right,bottom,left
+      padding = "10,10";
+      default-timeout = 5000;  # show 5s
+      layer = "overlay";       # show top layer (Wayland)
+      icons = true;
+
+      # Optional: urgency group css
+      "urgency=low" = {
+        background-color = "#313244";
+      };
+      "urgency=critical" = {
+        background-color = "#eba0ac";
+        text-color = "#111111";
+        default-timeout = 0;  # don't auto shadow
+      };
     };
   };
-};
-
+}
